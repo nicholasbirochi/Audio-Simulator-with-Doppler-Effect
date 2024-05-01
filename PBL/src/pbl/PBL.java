452a68ddfa99b673676c46891c4b49f880267fe2
Edgar <edgarseabra@outlook.com.br>
2024-04-30 21:45:27 -0300

    Separação e Adição de classe

--- PBL/src/pbl/PBL.java
+++ PBL/src/pbl/PBL.java
@@ -1,50 +1,7 @@
 package pbl;
 
-public class PBL
-{
-    //Funções Física:
-    public static double Doppler(double velObsAprox, double velFonteAprox)
-    {
-        double velSom = 340;
-        return (velSom + velObsAprox)/(velSom-velFonteAprox);
-    }
-    
-    public static long Fatorial (int number)
-    {
-        long value = 1;
-        for(int i = 1; i <= number; i++)
-        {
-            value *= i;
-        }
-        return value;
-    }
-    
-    public static double seno(double angulo)
-    {
-        
-        if(Math.abs(angulo) >= Math.PI * 2)
-        {
-            return seno(angulo % (Math.PI * 2));
-        }    
-
-     
-        double estimativa = 0;
-        double termo;
-
-        int i = 1;
-        while (i <= 20)
-        {
-            
-            int j = i%4 == 1? 1 : -1;
-            termo = (j) * Math.pow(angulo,i) / Fatorial(i);
-            estimativa += termo;
-            i += 2;
-        }
-        return estimativa;
-    }
-    
-    public static void main(String[] args)
-    {
-        System.out.println(seno(Math.PI));
+public class PBL {
+    public static void main(String[] args) {
+        System.out.println(funcoesFisica.seno(Math.PI/2));
     }
 }
---
+++ PBL/src/pbl/funcoesFisica.java
@@ -0,0 +1,42 @@
+package pbl;
+
+public class funcoesFisica {
+    public static double doppler(double velObsAprox, double velFonteAprox)
+    {
+        double velSom = 340;
+        return (velSom + velObsAprox)/(velSom-velFonteAprox);
+    }
+
+    public static long fatorial (int number)
+    {
+        long value = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            value *= i;
+        }
+        return value;
+    }
+
+    public static double seno(double angulo)
+    {
+
+        if(Math.abs(angulo) >= Math.PI * 2)
+        {
+            return seno(angulo % (Math.PI * 2));
+        }
+
+
+        double estimativa = 0;
+        double termo;
+
+        for (int i = 1; i < 20;)
+        {
+
+            int j = i%4 == 1? 1 : -1;
+            termo = (j) * Math.pow(angulo,i) / fatorial(i);
+            estimativa += termo;
+            i += 2;
+        }
+        return estimativa;
+    }
+}
