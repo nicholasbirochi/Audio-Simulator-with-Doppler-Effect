caffa8ad260985048c379e8cb22404bf1abf6450
Vítor Agostino Braghittoni <vitor.braghittoni@gmail.com>
2024-05-14 19:39:30 -0300

    Update PBL.java

--- PBL/src/pbl/PBL.java
+++ PBL/src/pbl/PBL.java
@@ -8,40 +8,13 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class PBL {
-    private static final Map<String, Double> MAPA_FREQUENCIA_NOTAS = new HashMap<>();
-    static {
-        MAPA_FREQUENCIA_NOTAS.put("Dó", 261.63);
-        MAPA_FREQUENCIA_NOTAS.put("Ré", 293.66);
-        MAPA_FREQUENCIA_NOTAS.put("Mi", 329.63);
-        MAPA_FREQUENCIA_NOTAS.put("Fá", 349.23);
-        MAPA_FREQUENCIA_NOTAS.put("Sol", 392.00);
-        MAPA_FREQUENCIA_NOTAS.put("Lá", 440.00);
-        MAPA_FREQUENCIA_NOTAS.put("Si", 493.88);
-    }
 
     public static void main(String[] args) {
         int taxaAmostragem = 44100; // Frequência de amostragem (Hz)
-        int duracaoSegundos = 5; // Duração do áudio em segundos
-        double amplitude = 0.5; // Amplitude do som (0.0 a 1.0)
         String caminhoDesktop = System.getProperty("user.home") + "/Desktop";
         Scanner scanner = new Scanner(System.in);
 
         try {
-            System.out.println("Escolha a nota musical:");
-            System.out.println("1. Dó\n2. Ré\n3. Mi\n4. Fá\n5. Sol\n6. Lá\n7. Si");
-            System.out.print("Digite o número da opção desejada: ");
-            int opcao = scanner.nextInt();
-            String nota = switch (opcao) {
-                case 1 -> "Dó";
-                case 2 -> "Ré";
-                case 3 -> "Mi";
-                case 4 -> "Fá";
-                case 5 -> "Sol";
-                case 6 -> "Lá";
-                case 7 -> "Si";
-                default -> throw new IllegalStateException("Opção inválida. Escolha uma opção de 1 a 7.");
-            };
-
             String caminhoAudio = caminhoDesktop + '/' + nota + ".wav";
             File arquivoAudio = new File(caminhoAudio);
             if (arquivoAudio.exists()) {
