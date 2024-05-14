a840fd458b81b7334c7986b899d63f9146522d29
Vítor Agostino Braghittoni <vitor.braghittoni@gmail.com>
2024-05-14 19:53:44 -0300

    Update Experimento.java

--- PBL/src/pbl/Experimento.java
+++ PBL/src/pbl/Experimento.java
@@ -1,1 +1,23 @@
+package pbl;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Experimento
+{
+  private double posicaoInicialFonte;
+  private double posicaoInicialObservador;
+  private double velocidadeFonte;
+  private double velocidadeObservador;
+  private double velocidadeSom;
+
+  public Experimento(double posicaoInicialFonte, double posicaoInicialObservador, double velocidadeFonte, double velocidadeObservador, double velocidadeSom)
+  {
+    this.posicaoInicialFonte = posicaoInicialFonte;
+    this.posicaoInicialObservador = posicaoInicialObservador;
+    this.velocidadeFonte = velocidadeFonte;
+    this.velocidadeObservador = velocidadeObservador;
+    this.velocidadeSom = velocidadeSom;
+  }
+  
+    
+}
