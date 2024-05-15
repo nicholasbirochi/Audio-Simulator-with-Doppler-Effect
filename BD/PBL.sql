a145465827f56ff65d3294a8790ca821ef7e3448
Nicholas Birochi <131502494+nicholasbirochi@users.noreply.github.com>
2024-05-15 12:05:14 -0300

    UpdateDataBase

--- BD/PBL.sql
+++ BD/PBL.sql
@@ -8,33 +8,41 @@ use PBL
 go
 create table cadastro
 (
-	usuarioID int primary key not null,
-	nome VARCHAR(100),
+	usuarioID int primary key identity not null,
+	nomePessoa VARCHAR(100),
 	email VARCHAR(100),
 	senha VARCHAR(100)
 )
 go
-
+	
 create table fonte
 (
-	fonteID int primary key not null,
+	fonteID int primary key identity not null,
 	frequencia decimal(10,5),
-	nome VARCHAR(100)
+	nomeFonte VARCHAR(100)
+)
+go
+	
+create table fonte
+(
+	ambienteID int primary key identity not null,
+	velocidadeSom decimal(10,5),
+	nomeAmbiente VARCHAR(100)
 )
 go
 
 create table simulacao
 (
-	experimentoID int primary key not null,
+	experimentoID int primary key identity not null,
 	experimentoNome VARCHAR(100),
 	velocidadeObservador decimal(10,5),
-	velocidadeFonte decimal(10,5),
-	velocidadeSom decimal(10,5),
-	posicaoInicialFonte decimal(10,5),
 	posicaoInicialObservador decimal(10,5),
+	velocidadeFonte decimal(10,5),
+	posicaoInicialFonte decimal(10,5),
+	ambienteID int,
 	fonteID int,
-	observadorID int
 )
 go
 alter table simulacao
-add constraint fk_fonteID foreign key (fonteID) references fonte
+add constraint fk_fonteID foreign key (fonteID) references fonte,
+    constraint fk_ambienteID foreign key (ambienteID) references ambiente
