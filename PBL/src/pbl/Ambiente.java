4e3697c6b42cdb432a146d32192721dd2f82eb51
Vítor Agostino Braghittoni <vitor.braghittoni@gmail.com>
2024-05-21 20:46:22 -0300

    Create Ambiente.java

---
+++ PBL/src/pbl/Ambiente.java
@@ -0,0 +1,30 @@
+package pbl;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Ambiente
+{
+  private double velocidadeSom;
+  private String nome;
+  private int id;
+
+  public Experimento(int id, String nome, double velocidadeSom)
+  {
+    this.velocidadeSom = velocidadeSom;
+    this.nome = nome;
+    this.id = id;
+  }
+  
+  public double getVelocidadeSom(){
+    return this.velocidadeSom;
+  }
+
+  public String getNome(){
+    return this.nome;
+  }
+
+  public int getId(){
+    return this.id;
+  }
+    
+}
