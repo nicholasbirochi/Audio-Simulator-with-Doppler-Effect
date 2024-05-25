5cb7574e616f58d85e6885f007a7a57adf704d20
Edgarcsr <edgarseabra@outlook.com.br>
2024-05-25 14:46:11 -0300

    Atualização do Front

--- PBLVisual/src/main/java/com/pbl/controller/Controlador.java
+++ PBLVisual/src/main/java/com/pbl/controller/Controlador.java
@@ -1,77 +1,27 @@
 package com.pbl.controller;
 
-import com.pbl.model.*;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.image.ImageView;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
-import java.sql.Connection;
 import com.pbl.model.ConexaoBD;
+import java.sql.Connection;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
 
 public class Controlador {
-    ConexaoBD conx = new ConexaoBD();
-    Connection connection = conx.getConexao();
+//    ConexaoBD conx = new ConexaoBD();
+//    Connection connection = conx.getConexao();
 
     @FXML
-    private Label welcomeText;
+    private Button buttonClose;
 
     @FXML
-    private ImageView imageTimbreAbelhaEletrica;
-    @FXML
-    private ImageView imageTimbreAlienigena;
-    // Adicionar outros ImageView para os timbres
-    @FXML
-    private Button buttonGerar;
-    @FXML
-    private Slider sliderDuracao;
-    private double duracaoAudio;
-
-    @FXML
-    public void initialize() {
-        // Inicializa a duração do áudio com o valor inicial do slider
-        duracaoAudio = sliderDuracao.getValue();
-
-        // Adiciona um ouvinte de mudança de valor ao slider
-        sliderDuracao.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                duracaoAudio = newValue.doubleValue();
-                System.out.println("A duração do áudio foi alterada para: " + duracaoAudio + " segundos");
-            }
-        });
+    private void handleCloseButtonAction() {
+        // Obtém a janela atual através do botão
+        Stage stage = (Stage) buttonClose.getScene().getWindow();
+        // Fecha a janela
+        stage.close();
     }
 
-    @FXML
-    protected void onButtonGerarClick() {
-        System.out.println("Botão Gerar foi pressionado!");
-        try {
-            Ambiente ambiente = new Ambiente(1, "ar", 330);
-            Timbre timbre = new TimbreAbelhaEletrica();
-            Fonte fonte = new Fonte(4, 440, timbre);
-            // Use a variável duracaoAudio para definir a duração do experimento
-            Experimento exp = new Experimento(1, "Experimento de teste",25,1, -10, 0, duracaoAudio, 44100, ambiente, fonte);
-            String caminhoDesktop = System.getProperty("user.home") + "/Desktop";
-            PBL.criarArquivo(caminhoDesktop + "/Audio.wav", exp);
-            System.out.println("Arquivo gerado com sucesso!");
-        } catch (IOException | UnsupportedAudioFileException e) {
-            System.out.println("Erro ao criar arquivo de áudio: " + e.getMessage());
-        }
-    }
-
-    @FXML
-    protected void onTimbreAbelhaEletricaClick() {
-        welcomeText.setText("Timbre Abelha Elétrica selecionado!");
-    }
-
-    @FXML
-    protected void onTimbreAlienigenaClick() {
-        welcomeText.setText("Timbre Alienígena selecionado!");
-    }
-
-    // Adicionar métodos para os outros botões
 }
