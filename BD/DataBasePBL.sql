3d59a0e117840647530e3f12d1a0a0514cd53f22
Nicholas Birochi <131502494+nicholasbirochi@users.noreply.github.com>
2024-05-27 20:58:12 -0300

    Update DataBasePBL.sql

--- BD/DataBasePBL.sql
+++ BD/DataBasePBL.sql
@@ -12,18 +12,10 @@ create table fonte
 	fonteNome VARCHAR(50) primary key,
 	timbreNome VARCHAR(50) not null,
 	posicaoInicialFonte decimal(10,5) not null,
-	posicaoLateralFonte decimal(10,5) not null,
 	velocidadeFonte decimal(10,5) not null,
+	posicaoLateral decimal(10,5) not null,
 	potencia decimal(10,5) not null,
 	frequencia decimal(10,5) not null
-)
-go
-
-create table observador
-(
-	observadorNome VARCHAR(50) primary key,
-	velocidadeObservador decimal(10,5) not null,
-	posicaoInicialObservador decimal(10,5) not null
 )
 go
 	
@@ -45,6 +37,8 @@ create table experimento
 	experimentoNome VARCHAR(50) primary key,
 	tempoDuracao decimal(10,5) not null,
 	taxaAmostragem int not null,
+	velocidadeObservador decimal(10,5) not null,
+	posicaoInicialObservador decimal(10,5) not null,
 	ambienteNome VARCHAR(50) not null,
 	fonteNome VARCHAR(50) not null,
 	observadorNome VARCHAR(50) not null
@@ -52,7 +46,6 @@ create table experimento
 go
 alter table experimento
 add constraint fk_fonteNome foreign key (fonteNome) references fonte,
-	constraint fk_observadorNome foreign key (observadorNome) references observador,
 	constraint fk_ambienteNome foreign key (ambienteNome) references ambiente
 
 go
