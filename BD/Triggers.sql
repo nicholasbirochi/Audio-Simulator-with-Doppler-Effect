70305f978d146c2c9e03d1e03a3355cd66af9c2b
Nicholas Birochi <131502494+nicholasbirochi@users.noreply.github.com>
2024-05-27 21:14:59 -0300

    Update TriggersDEL.sql

--- BD/Triggers.sql
+++ BD/Triggers.sql
@@ -49,39 +49,37 @@ go
 
 create or alter trigger trg_delAmbiente
 ON ambiente
-INSTEAD OF INSERT
+INSTEAD OF DELETE
 AS
 BEGIN
 	Declare @nome VARCHAR(50) = (select ambienteNome from ambiente)
     -- Verifica se o valor já existe na tabela
     IF EXISTS (SELECT 1 FROM ambiente WHERE @nome IN (SELECT ambienteNome FROM deleted))
     BEGIN
-        -- Lança um erro para impedir a inserção
-        RAISERROR('Já existe esse Observador.', 16, 1);
+	-- Permite o delete se o valor existir:
+        DELETE INTO ambiente SELECT * FROM deleted;
     END
     ELSE
     BEGIN
-        -- Permite a inserção se o valor não existir
-        INSERT INTO ambiente SELECT * FROM deleted;
+	RAISERROR('Esse Ambiente não existe', 16, 1);
     END
 END
 go
 	    
 create or alter trigger trg_delFonte
 ON fonte
-INSTEAD OF INSERT
+INSTEAD OF DELETE
 AS
 BEGIN
 	Declare @nome VARCHAR(50) = (select fonteNome from fonte)
     -- Verifica se o valor já existe na tabela
     IF EXISTS (SELECT 1 FROM fonte WHERE @nome IN (SELECT fonteNome FROM deleted))
     BEGIN
-        -- Lança um erro para impedir a inserção
-        RAISERROR('Já existe esse Observador.', 16, 1);
+	-- Permite o delete se o valor existir:
+        DELETE INTO fonte SELECT * FROM deleted;
     END
     ELSE
     BEGIN
-        -- Permite a inserção se o valor não existir
-        INSERT INTO fonte SELECT * FROM deleted;
+	RAISERROR('Essa Fonte não existe', 16, 1);
     END
 END
